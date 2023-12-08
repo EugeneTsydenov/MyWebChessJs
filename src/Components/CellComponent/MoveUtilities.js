6c940828402a9a5413e98cf01eb61a28c7892245
beginnerwebprogrammer <ecydenov110@gmail.com>
2023-12-08 23:01:30 +0800

    Added audio before move

--- src/Components/CellComponent/MoveUtilities.js
+++ src/Components/CellComponent/MoveUtilities.js
@@ -1,3 +1,18 @@
+import sound from '../../public/Sounds/1572965944_6a897efd83627af.mp3'
+import {Colors} from "../../data/Colors.js";
+import transformPawn from "./TransformPawn.js";
+import {FigureNames} from "../../data/FigureNames.js";
+
 export function selectedCellMove(cell, selectedCell) {
-    selectedCell.moveFigure(cell);
+    console.log(cell, selectedCell)
+    const s = new Audio(sound);
+    s.volume = 0.2;
+    s.play();
+    // if(selectedCell?.figure?.name === FigureNames.PAWN) {
+    //     if(selectedCell?.figure?.color === Colors.WHITE && cell.x === 0) {
+    //         console.log(2)
+    //         transformPawn()
+    //     }
+    // }
+    // selectedCell.moveFigure(cell);
 }
---
+++ src/Components/CellComponent/TransformPawn.js
@@ -0,0 +1,12 @@
+// export default function transformPawn() {
+//     console.log(1)
+//   showPieceFoPawPromotion()
+// }
+//
+// function showPieceFoPawPromotion() {
+//     const el = document.querySelector('.board')
+//     console.log(el)
+//     const checkboxes = document.createElement('div');
+//     checkboxes.className = 'last-move';
+//     el.appendChild(checkboxes);
+// }
